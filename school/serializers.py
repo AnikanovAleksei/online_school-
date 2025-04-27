c96c703daa037412fe5d49ef9e2e820ff1bf86b6
Aleksei Anikanov <python_work@bk.ru>
2025-04-27 16:56:49 +0300

    Done

--- school/serializers.py
+++ school/serializers.py
@@ -2,13 +2,20 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+
+    lessons = LessonSerializer(source="lessons.all", many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['name', 'description', 'lessons', 'lessons_count']
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from school.models import Course, Lesson
 
 
 class SchoolUser(AbstractUser):
@@ -19,3 +20,27 @@ class SchoolUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payment(models.Model):
+
+    PAYMENT_METHOD_CHOICES = [
+        ('cash', 'Наличные'),
+        ('transfer', 'Перевод на счёт'),
+    ]
+
+    user = models.ForeignKey(SchoolUser, on_delete=models.CASCADE, verbose_name='Пользователь', related_name='payments')
+    payment_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата оплаты')
+    paid_course = models.ForeignKey(Course, on_delete=models.SET_NULL, blank=True, null=True,
+                                    verbose_name='Оплаченный курс', related_name='payments')
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, verbose_name='Оплаченный урок', blank=True,
+                                    null=True, related_name='payments')
+    amount = models.IntegerField(verbose_name='Сумма оплаты')
+    payment_method = models.CharField(max_length=15, choices=PAYMENT_METHOD_CHOICES, verbose_name='Способ оплаты')
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
+
+    def __str__(self):
+        return f"Платёж {self.user} на сумму {self.amount} ({self.payment_date})"
